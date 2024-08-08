510c9287a5f76c7847210d0f4bde1e45c6e58bf8
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2024-08-08 19:51:19 +0545

    Dashboard icons for BooksView made functional

--- src/View/BooksView.java
+++ src/View/BooksView.java
@@ -45,12 +45,12 @@ public class BooksView extends javax.swing.JFrame {
         jLabel14 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jPanel1 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
+        DashHomeLabel = new javax.swing.JLabel();
         DashLogo = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
+        DashCustomersDetailsLabel = new javax.swing.JLabel();
+        DashBorrowDetailsLabel = new javax.swing.JLabel();
+        DashCustomersPageLabel = new javax.swing.JLabel();
+        DashBorrowPageLabel = new javax.swing.JLabel();
         LogoutButton = new javax.swing.JButton();
         jLabel15 = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
@@ -119,9 +119,14 @@ public class BooksView extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setPreferredSize(new java.awt.Dimension(238, 696));
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel2.setIcon(new FlatSVGIcon("homeicon.svg"));
-        jLabel2.setText("Home");
+        DashHomeLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        DashHomeLabel.setIcon(new FlatSVGIcon("homeicon.svg"));
+        DashHomeLabel.setText("Home");
+        DashHomeLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                DashHomeLabelMouseClicked(evt);
+            }
+        });
 
         DashLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/BooksPics/logo_small.png"))); // NOI18N
         DashLogo.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -130,22 +135,42 @@ public class BooksView extends javax.swing.JFrame {
             }
         });
 
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        jLabel4.setIcon(new FlatSVGIcon("customers1.svg"));
-        jLabel4.setText("Customer Details");
+        DashCustomersDetailsLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        DashCustomersDetailsLabel.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        DashCustomersDetailsLabel.setIcon(new FlatSVGIcon("customers1.svg"));
+        DashCustomersDetailsLabel.setText("Customer Details");
+        DashCustomersDetailsLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                DashCustomersDetailsLabelMouseClicked(evt);
+            }
+        });
 
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel5.setIcon(new FlatSVGIcon("bookBorrowed1.svg"));
-        jLabel5.setText("Borrow Details");
+        DashBorrowDetailsLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        DashBorrowDetailsLabel.setIcon(new FlatSVGIcon("bookBorrowed1.svg"));
+        DashBorrowDetailsLabel.setText("Borrow Details");
+        DashBorrowDetailsLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                DashBorrowDetailsLabelMouseClicked(evt);
+            }
+        });
 
-        jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel6.setIcon(new FlatSVGIcon("customerPage.svg"));
-        jLabel6.setText("Customers Page");
+        DashCustomersPageLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        DashCustomersPageLabel.setIcon(new FlatSVGIcon("customerPage.svg"));
+        DashCustomersPageLabel.setText("Customers Page");
+        DashCustomersPageLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                DashCustomersPageLabelMouseClicked(evt);
+            }
+        });
 
-        jLabel7.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel7.setIcon(new FlatSVGIcon("borrowPage.svg"));
-        jLabel7.setText("Borrow Page");
+        DashBorrowPageLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        DashBorrowPageLabel.setIcon(new FlatSVGIcon("borrowPage.svg"));
+        DashBorrowPageLabel.setText("Borrow Page");
+        DashBorrowPageLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                DashBorrowPageLabelMouseClicked(evt);
+            }
+        });
 
         LogoutButton.setBackground(new java.awt.Color(255, 153, 0));
         LogoutButton.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
@@ -168,11 +193,11 @@ public class BooksView extends javax.swing.JFrame {
                         .addGap(47, 47, 47)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(LogoutButton, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 186, Short.MAX_VALUE)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                            .addComponent(DashBorrowDetailsLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(DashCustomersPageLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(DashCustomersDetailsLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 186, Short.MAX_VALUE)
+                            .addComponent(DashHomeLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(DashBorrowPageLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(DashLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 209, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -184,15 +209,15 @@ public class BooksView extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(DashLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(35, 35, 35)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(DashHomeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(35, 35, 35)
-                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(DashCustomersDetailsLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(35, 35, 35)
-                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(DashCustomersPageLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(35, 35, 35)
-                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(DashBorrowDetailsLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(35, 35, 35)
-                .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(DashBorrowPageLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 590, Short.MAX_VALUE)
                 .addComponent(LogoutButton, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(49, 49, 49))
@@ -323,6 +348,41 @@ public class BooksView extends javax.swing.JFrame {
         home.setVisible(true);
     }//GEN-LAST:event_DashLogoMouseClicked
 
+    private void DashHomeLabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_DashHomeLabelMouseClicked
+        // TODO add your handling code here:
+        this.dispose();
+        HomePage home = new HomePage();
+        home.setVisible(true);
+    }//GEN-LAST:event_DashHomeLabelMouseClicked
+
+    private void DashCustomersDetailsLabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_DashCustomersDetailsLabelMouseClicked
+        // TODO add your handling code here:
+                this.dispose();
+        CustomerDetails cc = new CustomerDetails();
+        cc.setVisible(true);
+    }//GEN-LAST:event_DashCustomersDetailsLabelMouseClicked
+
+    private void DashCustomersPageLabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_DashCustomersPageLabelMouseClicked
+        // TODO add your handling code here:
+        this.dispose();
+        CustomerView cv = new CustomerView();
+        cv.setVisible(true);
+    }//GEN-LAST:event_DashCustomersPageLabelMouseClicked
+
+    private void DashBorrowDetailsLabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_DashBorrowDetailsLabelMouseClicked
+        // TODO add your handling code here:
+        this.dispose();
+        BookingDetails bd = new BookingDetails();
+        bd.setVisible(true);
+    }//GEN-LAST:event_DashBorrowDetailsLabelMouseClicked
+
+    private void DashBorrowPageLabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_DashBorrowPageLabelMouseClicked
+        // TODO add your handling code here:
+                this.dispose();
+        BooksView bv = new BooksView();
+        bv.setVisible(true);
+    }//GEN-LAST:event_DashBorrowPageLabelMouseClicked
+
     /**
      * @param args the command line arguments
      */
@@ -359,6 +419,11 @@ public class BooksView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel DashBorrowDetailsLabel;
+    private javax.swing.JLabel DashBorrowPageLabel;
+    private javax.swing.JLabel DashCustomersDetailsLabel;
+    private javax.swing.JLabel DashCustomersPageLabel;
+    private javax.swing.JLabel DashHomeLabel;
     private javax.swing.JLabel DashLogo;
     private javax.swing.JButton LogoutButton;
     private javax.swing.JButton jButton2;
@@ -372,11 +437,6 @@ public class BooksView extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel14;
     private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel16;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
