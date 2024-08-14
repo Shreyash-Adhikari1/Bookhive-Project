fcbeab2c46272365a90ca1411766d40ef32f8e73
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2024-08-14 19:17:09 +0545

    Customer View Model completed

--- src/Model/BookViewModel.java
+++ src/Model/BookViewModel.java
@@ -9,5 +9,44 @@ package Model;
  * @author Acer
  */
 public class BookViewModel {
+    private final int bookID;
+    private final String bookName;
+    private final String customerName;
+    private final String contactNumber;
+    private final String issuedDate;
+    private final String  returnDate;
     
+    public BookViewModel(int bookID, String bookName, String customerName, String contactNumber, String issuedDate, String returnDate){
+        this.bookID=bookID;
+        this.bookName=bookName;
+        this.customerName=customerName;
+        this.contactNumber=contactNumber;
+        this.issuedDate=issuedDate;
+        this.returnDate=returnDate;
+    }
+    
+    public int getBookID(){
+        return bookID;
+    }
+    
+    public String getBookName(){
+        return bookName;
+    }
+    
+    public String getCustomerName(){
+        return customerName;
+    }
+    
+    public String getContactNumber(){
+        return contactNumber;
+    }
+    
+    public String getIssuedDate(){
+        return issuedDate;
+    }
+    
+    public String getReturnDate(){
+        return returnDate;
+    }
 }
+
