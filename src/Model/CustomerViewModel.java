c0fb624b556e11aa0915beb8a3ddb37540aa0fc6
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2024-08-15 13:24:24 +0545

    Minor updates || bug fixes

--- src/Model/CustomerViewModel.java
+++ src/Model/CustomerViewModel.java
@@ -9,5 +9,43 @@ package Model;
  * @author Acer
  */
 public class CustomerViewModel {
+    private final int customerID;
+    private final String customerName;
+    private final String contactNumber;
+    private final String email;
+    private final String address;
+    private final String gender;
     
+    public CustomerViewModel(int customerID, String customerName, String contactNumber, String email, String address, String gender){
+        this.customerID=customerID;
+        this.customerName= customerName;
+        this.contactNumber= contactNumber;
+        this.email=email;
+        this.address=address;
+        this.gender=gender;
+    }
+    
+    public int getCustomerID(){
+        return customerID;
+    }
+    
+    public String getCustomerName(){
+        return customerName;
+    }
+    
+    public String getContactNumber(){
+        return contactNumber;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public String getGender(){
+        return gender;
+    }
 }
