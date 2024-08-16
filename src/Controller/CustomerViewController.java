4216e38197c72014b82c8424e787621004c09143
Anupkatwal1 <159704755+Anupkatwal1@users.noreply.github.com>
2024-08-16 22:22:13 +0530

    Updates to CustomerView Controller

--- src/Controller/CustomerViewController.java
+++ src/Controller/CustomerViewController.java
@@ -3,11 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controller;
+import View.CustomerDetails;
+import View.CustomerView;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author ASUS
  */
 public class CustomerViewController {
+    private final CustomerView view;
     
+        public CustomerViewController(CustomerView view){
+        this.view=view;
+        
+        view.getUpdateDetailsButton().addActionListener(new UpdateCustomerButtonListener());
+        
+        view.getDeleteCustomerButton().addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                DeleteCustomerbutotnMouseClicked(evt);
+            }
+        });
+        
+        }
 }
