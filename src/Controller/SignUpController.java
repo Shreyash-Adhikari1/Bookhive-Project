4c5ed365d0c2958bf4447003f6a4daab146ae88b
Anupkatwal1 <159704755+Anupkatwal1@users.noreply.github.com>
2024-08-16 22:12:07 +0530

    SignUp Controller made

--- src/Controller/SignUpController.java
+++ src/Controller/SignUpController.java
@@ -3,13 +3,59 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controller;
+import View.LoginPage;
 import View.SignUp;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Acer
  */
 public class SignUpController {
-    
+    private final SignUp sign;
+
+    public SignUpController(SignUp sign){
+        this.sign=sign;
+
+        sign.getRegisterButton().addActionListener(e -> SignupButtonClicked());
+    } 
+    private void SignupButtonClicked(){
+         String RegNO  = sign.getRegNoField().getText().trim();
+        String email = sign.getEmailField().getText().trim();
+        String password = new String (sign.getPasswordField().getPassword()).trim();
+        String confirm = new String (sign.getConfirmPassField().getPassword()).trim();
+
+        // to ensure text feilds not empty
+if(RegNO.equals("")){
+    JOptionPane.showMessageDialog(sign, "Enter Registration Number!");
+
+}
+else if(email.equals("")){
+    JOptionPane.showMessageDialog(sign,"Enter Email!");
+    return;
+}
+else if(!email.endsWith("@gmail.com")){
+            JOptionPane.showMessageDialog(sign,"Email must contain '@gmail.com'");
+            return;
+        }
+else if (password.equals("")){
+        JOptionPane.showMessageDialog(sign,"Enter Password!");
+    return;
+}
+else if (confirm.equals("")){
+        JOptionPane.showMessageDialog(sign,"Enter Confirmation Password!");
+    return;
+}
+else if(!confirm.equals( password)){
+JOptionPane.showMessageDialog(sign, "Passwords Dont Match");
+}
+else{
+JOptionPane.showMessageDialog(sign, "Registration Successfull");
+sign.dispose();
+LoginPage log = new LoginPage();
+log.setVisible(true);
+
+}
+    }
     
 }
